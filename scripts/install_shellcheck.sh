d47e03774b4b69246368994f0e221df4833edb36
Hugo Duksis <duksis@gmail.com>
2021-02-28 13:39:27 +0100

    shellchek has moved - use new dowload source

--- scripts/install_shellcheck.sh
+++ scripts/install_shellcheck.sh
@@ -6,7 +6,7 @@
 ## manually
 
 if [ ! -x shellcheck-latest/shellcheck ]; then
-  wget -qO- "https://storage.googleapis.com/shellcheck/shellcheck-latest.darwin.x86_64.tar.xz" | tar -xJv
+  wget -qO- "https://github.com/koalaman/shellcheck/releases/download/latest/shellcheck-latest.darwin.x86_64.tar.xz" | tar -xJv
 fi
 
 shellcheck-latest/shellcheck --version
